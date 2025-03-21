a3d0c5dbee809daab6f4cbf9d630b74b2a09a72f
bringvotrevin <bringvotrevin@gmail.com>
2025-03-21 17:30:22 +0900

    chore: change text in client.tsx

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -8,42 +8,42 @@ const Client = () => {
       <h3 className='fraunces-bold text-(--grayish-blue) tracking-[0.2em]'>
         CLIENT TESTIMONIALS
       </h3>
-      <ul className='flex'>
-        <li className='flex flex-col gap-10 items-center w-72'>
+      <ul className='flex gap-7'>
+        <li className='flex flex-col gap-10 items-center w-68'>
           <img src={Emily} alt='Emily R' className='rounded-full w-16' />
           <p className='text-center text-sm font-semibold text-(--dark-grayish-blue)'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            repudiandae aperiam eum dolorem libero. Vero tempora nihil,
+            We put our trust in Sunnyside and they delivered, making sure our
+            needs were met and deadlines were always hit.
           </p>
           <div className='text-center'>
-            <div className='fraunces-bold'>Emily R.</div>
-            <div className='font-semibold text-(--dark-grayish-blue) text-sm'>
+            <div className='fraunces-bold text-base'>Emily R.</div>
+            <div className='font-semibold text-(--grayish-blue) text-xs'>
               Marketing Director
             </div>
           </div>
         </li>
-        <li className='flex flex-col gap-10 items-center w-72'>
+        <li className='flex flex-col gap-10 items-center w-68'>
           <img src={Thomas} alt='Emily R' className='rounded-full w-16' />
           <p className='text-center text-sm font-semibold text-(--dark-grayish-blue)'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            repudiandae aperiam eum dolorem libero. Vero tempora nihil,
+            Sunnyside's enthusiasm coupled with their keen interest in our
+            brand's success made it a satisfying and enjoyable experience.
           </p>
           <div className='text-center'>
-            <div className='fraunces-bold'>Thomas S.</div>
-            <div className='font-semibold text-(--dark-grayish-blue) text-sm'>
+            <div className='fraunces-bold text-base'>Thomas S.</div>
+            <div className='font-semibold text-(--grayish-blue) text-xs'>
               Cheif Operationg Officer
             </div>
           </div>
         </li>
-        <li className='flex flex-col gap-10 items-center w-72'>
+        <li className='flex flex-col gap-10 items-center w-68'>
           <img src={Jennie} alt='Emily R' className='rounded-full w-16' />
           <p className='text-center text-sm font-semibold text-(--dark-grayish-blue)'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            repudiandae aperiam eum dolorem libero. Vero tempora nihil,
+            Incredible end result! Our sales increased over 400% when we worked
+            with Sunnyside. <br /> Highly recommended!
           </p>
           <div className='text-center'>
-            <div className='fraunces-bold'>Jennie F.</div>
-            <div className='font-semibold text-(--dark-grayish-blue) text-sm'>
+            <div className='fraunces-bold text-base'>Jennie F.</div>
+            <div className='font-semibold text-(--grayish-blue) text-xs'>
               Business Owner
             </div>
           </div>
